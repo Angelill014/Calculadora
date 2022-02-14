2a4ed6e6056e6315411079f619af9fcf8818180a
Miriam <mirianmc1998@gmail.com>
2022-02-14 13:46:54 +0100

    Creadalaclasecalculadora

---
+++ Calculadora.cs
@@ -0,0 +1,60 @@
+                                //14-02-22//
+using System;
+
+public class Calculadora
+{
+    public void Suma(String[] args)
+    {
+       int numero1;
+       int numero2;
+       int resultado;
+       Console.WriteLine("Introduzca el primer numero");
+      numero1 = Int32.Parse (Console.ReadLine());
+      Console.WriteLine("Introduzca el segundo numero");
+      numero2 = Int32.Parse (Console.ReadLine());
+      resultado = numero1 + numero2;
+      Console.WriteLine(resultado);
+    }
+
+      public void Resta (String[] args)
+       {
+          int numero1;
+          int numero2;
+          int resultado;
+          Console.WriteLine("Introduzca el primer numero");
+          numero1 = Int32.Parse (Console.ReadLine());
+          Console.WriteLine("Introduzca el segundo numero");
+          numero2 = Int32.Parse(Console.ReadLine());
+          resultado = numero1 - numero2;
+          Console.WriteLine(resultado);
+
+        }
+    
+    public void Multiplicacion (String[] args)
+    {
+       int numero1;
+       int numero2;
+       int resultado;
+       Console.WriteLine("Introduzca el primer numero");
+       numero1 = Int32.Parse (Console.ReadLine());
+       Console.WriteLine("Introduzca el segundo numero");
+       numero2 = Int32.Parse(Console.ReadLine());
+       resultado = numero1 * numero2;
+       Console.WriteLine(resultado);
+    }
+
+      public void Division (String[] args)
+      {
+          int numero1;
+          int numero2;
+          int resultado;
+          Console.WriteLine("Introduzca el primer numero");
+          numero1 = Int32.Parse(Console.ReadLine());
+          Console.WriteLine("Introduzca el segundo numero");
+          numero2 = Int32.Parse(Console.ReadLine());
+          resultado = numero1 / numero2;
+          Console.WriteLine(resultado);
+      }
+
+}
+    
