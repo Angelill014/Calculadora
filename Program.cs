52cc17d77d04001045d6a5f96c05f76e949c690d
Miriam <mirianmc1998@gmail.com>
2022-02-24 11:10:20 +0100

    CambiosMenu

--- Program.cs
+++ Program.cs
@@ -2,75 +2,22 @@
 
 public class Program
 {
-    public static void Main(String[] args)
-    {
-      Calculadora calculadora = new Calculadora()
-      Operación operación = new Operación();
-      int opción = 0;
-      while (opción != 5)
-      {
-          opción = Menu();
-          if (opción == 1)
-          {
-              Console.Clear();
-              operación = CrearOperación;
-              calculadora.introducirOperación(operación);
-          }
-          else if (opción == 2)
-          {
-              Console.Clear();
-              operación = CrearOperación;
-              calculadora.introducirOperación(operación);
-          }
-          else if (opción == 3)
-          {
-              Console.Clear();
-              operación = CrearOperación;
-              calculadora.introducirOperación(operación);
-          }
-          else if (opción == 4)
-          {
-              Console.Clear();
-              operación = CrearOperación;
-              calculadora.introducirOperación(operación);
-          }
-      }     
-    }
-    public void Menu()
-    {
-        Console.WriteLine("BIENVENIDO A NUESTRA CALCULADORA");
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine("Seleccione la opción que desea");
-        Console.WriteLine("1. - Sumar");
-        Console.WriteLine("2. - Restar");
-        Console.WriteLine("3. - Multiplicar");
-        Console.WriteLine("4. - Dividir");
-        int opción = int.Parse(Console.ReadLine());
-        while(RevisiónOpcionMenu(opción) == false)
+   public static void Main(String[] args)
+   {
+     EscribirMenu();
+   }
+        public static void EscribirMenu()
         {
-             Console.Clear();
-                Console.WriteLine("Introduzca una opción valida por favor:");
-               opción = Int32.Parse(Console.ReadLine());
+         Console.WriteLine("Bienvenido A Mi Calculadora");
+         Console.WriteLine("---------------------------");
+         Console.WriteLine("Seleccione la opcion que desea");
+         Console.WriteLine("1. Suma");
+         Console.WriteLine("2. Resta");
+         Console.WriteLine("3. Multiplicacion");
+         Console.WriteLine("4. Division");
+         Console.WriteLine("5. Salir");
+         int opcion = Int32.Parse(Console.ReadLine());
         }
-        return opción;
-    }
-
-    public static bool RevisiónOpcionMenu(int opción)
-    {
-        if (opción == 1)
-        return true;
-         else if (opción == 2)
-                return true;
-            else if (opción == 3)
-                return true;
-            else if (opción == 4)
-                return true;
-            else
-                return false;
-    }
-    public static operación CrearOperación()
-    {
-        
-    }
 }
-
+    
+      
