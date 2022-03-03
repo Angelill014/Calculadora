7bc9ef578f58827097738a635dd3c6ce49468a7a
Miriam <mirianmc1998@gmail.com>
2022-03-03 09:35:47 +0100

    Actualizado Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Program
+namespace MiCalculadora
 {
-   public static void Main(String[] args)
+   internal class Principal
    {
-     EscribirMenu();
-   }
-        public static void EscribirMenu()
+
+   
+        public static void Main(String[] args)
         {
-         Console.WriteLine("Bienvenido A Mi Calculadora");
-         Console.WriteLine("---------------------------");
-         Console.WriteLine("Seleccione la opcion que desea");
-         Console.WriteLine("1. Suma");
-         Console.WriteLine("2. Resta");
-         Console.WriteLine("3. Multiplicacion");
-         Console.WriteLine("4. Division");
-         Console.WriteLine("5. Salir");
-         int opcion = Int32.Parse(Console.ReadLine());
-        }
+          int num1;
+          int num2;
+          int resultado;
+          Calculadora calculadora = null;
+
+          Console.WriteLine("Bienvenido a Nuestra Calculadora");
+          Console.WriteLine("--------------------------------");
+          Console.WriteLine("Introduzca el primer numero");
+          num1 = Int32.Parse(Console.ReadLine());
+          Console.WriteLine("Introduzca el segundo numero");
+          num2 = Int32.Parse(Console.ReadLine());
+          Console.WriteLine("Seleccione una opcion de calculo");
+          Console.WriteLine("1. Suma");
+          Console.WriteLine("2. Resta");
+          Console.WriteLine("3. Multiplicacion");
+          Console.WriteLine("4. Division");
+          int opcion = Int32.Parse(Console.ReadLine());
+          Calculadora.setNumero1(num1);
+
+         switch (opcion)
+         {
+           case 1:
+           resultado = num1 + num2;
+           Console.WriteLine ("El resultado es: " + Calculadora.Suma ());
+           break;
+         
+           case 2:
+           resultado = num1 - num2;
+           Console.WriteLine ("El resultado es: " + Calculadora.Resta ());
+           break;
+
+           case 3:
+           resultado = num1 * num2;
+           Console.WriteLine ("El resultado es: " + Calculadora.Multiplicacion ());
+           break;
+
+           case 4:
+           resultado = num1 / num2;
+           Console.WriteLine ("El resultado es: " + Calculadora.Division ());
+           break;
+
+           default:
+           Console.WriteLine("La cantidad es incorrecta");
+           Console.ReadLine();
+           Console.WriteLine("La cantidad es correcta");
+           break;
+         }
+         Console.ReadLine();
+      }  
+      
+   }
+    
 }
+
+   
+
+
     
       
