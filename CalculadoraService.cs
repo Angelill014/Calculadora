0d292f4b4a5732d40a755165bf63951df42c9f50
Mona_Lisa <monalisa@davinci.com>
2022-03-10 11:00:21 +0100

    Creada Clase CS

--- CalculadoraService.cs
+++ CalculadoraService.cs
@@ -5,24 +5,44 @@
         
         public  void Ejecutar()
         {
-        int num1;
-        int num2;
-        int resultado;
+            int num1;
+            int num2;
+            int resultado;
 
 
-        Console.WriteLine("Bienvenido a Nuestra Calculadora");
-        Console.WriteLine("--------------------------------");
-        Console.WriteLine("Introduzca el primer numero");
-        num1 = Int32.Parse(Console.ReadLine());
-        Console.WriteLine("Introduzca el segundo numero");
-        num2 = Int32.Parse(Console.ReadLine());
-        Console.WriteLine("Seleccione una opción de cálculo");
-        Console.WriteLine("1. Suma");
-        Console.WriteLine("2. Resta");
-        Console.WriteLine("3. Multiplicacion");
-        Console.WriteLine("4. Division");
-        int opcion = Int32.Parse(Console.ReadLine());
-        }
+            Console.WriteLine("Bienvenido a Nuestra Calculadora");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Introduzca el primer numero");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca el segundo numero");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Seleccione una opción de cálculo");
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicacion");
+            Console.WriteLine("4. Division");
+            int opcion = Int32.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Su resultado es" + " " + Calculadora.Suma(num1, num2));
+                    break;
+
+                case 2:
+                    Console.WriteLine("Su resultado es" + " " + Calculadora.Resta(num1, num2));
+                    break;
+
+                case 3:
+                    Console.WriteLine("Su resultado es" + " " + Calculadora.Multiplicacion(num1, num2));
+                    break;
+
+                case 4:
+                    Console.WriteLine("Su resultado es" + " " + Calculadora.Division(num1, num2));
+                    break;
+
+            }
+    }
 
         
     }
--- Program.cs
+++ Program.cs
@@ -4,54 +4,14 @@ namespace ProyectoCalculadora
 {
    internal class Principal
    {
-        CalculadoraService calculadoraService = new CalculadoraService();
-        public Principal()
-        {
-            Console.WriteLine(calculadoraService.Ejecutar);
-        }
+        
    
         public static void Main(string[] args)
         {
-          Calculadora calculadora = new Calculadora();
-          int num1;
-          int num2;
-          int resultado;
-         
+            CalculadoraService calculadoraService = new CalculadoraService();
 
-          Console.WriteLine("Bienvenido a Nuestra Calculadora");
-          Console.WriteLine("--------------------------------");
-          Console.WriteLine("Introduzca el primer numero");
-          num1 = Int32.Parse(Console.ReadLine());
-          Console.WriteLine("Introduzca el segundo numero");
-          num2 = Int32.Parse(Console.ReadLine());
-          Console.WriteLine("Seleccione una opción de cálculo");
-          Console.WriteLine("1. Suma");
-          Console.WriteLine("2. Resta");
-          Console.WriteLine("3. Multiplicacion");
-          Console.WriteLine("4. Division");
-          int opcion = Int32.Parse(Console.ReadLine());
-
-
-        switch (opcion)
-        {
-            case 1:
-                Console.WriteLine("Su resultado es" + " " + Calculadora.Suma(num1, num2));
-                break;
-
-            case 2:
-                Console.WriteLine("Su resultado es" + " " + Calculadora.Resta(num1, num2));
-                break;
-
-            case 3:
-                Console.WriteLine("Su resultado es" + " " + Calculadora.Multiplicacion(num1, num2));
-                break;
-
-            case 4:
-                Console.WriteLine("Su resultado es" + " " + Calculadora.Division(num1, num2));
-                break;
-                    
-        }
-
+            calculadoraService.Ejecutar();
+          
            
         }
         
