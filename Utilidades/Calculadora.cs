56adec6eab543de3f3456eaa8234010e22105b0f
Angelill014 <angelsanchezdelacruz@gmail.com>
2022-03-17 11:54:26 +0100

    Listado

--- Utilidades/Calculadora.cs
+++ Utilidades/Calculadora.cs
@@ -5,10 +5,22 @@ namespace Utilidades
 {
     public  class Calculadora
     {
+        
         public Calculadora()
         {
         
         }
+
+        List<string> firstlist = new List<string> ()
+        {
+            "Suma",
+            "Resta",
+            "Multiplicacion",
+            "Division"
+        };
+
+        List<string> calculadora = new List<string>();
+        calculadora.Add(InvalidTimeZoneException )
     
         public int Suma(int numero1, int numero2)
         {
